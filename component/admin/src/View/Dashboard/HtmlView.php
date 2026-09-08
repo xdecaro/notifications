f9e0d468569156318353f7a846fec773a1e0cf6b
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-08 23:56:31 +0200

    Add Notifications dashboard data

--- component/admin/src/View/Dashboard/HtmlView.php
+++ component/admin/src/View/Dashboard/HtmlView.php
@@ -3,16 +3,29 @@ namespace Xdecaro\Component\Notifications\Administrator\View\Dashboard;
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use Joomla\CMS\Toolbar\ToolbarHelper;
 use Xdecaro\Core\Asset\AssetService;
 
 final class HtmlView extends BaseHtmlView
 {
+    /** @var array */
+    public $stats = [];
+
+    /** @var array */
+    public $recent = [];
+
     public function display($tpl = null): void
     {
+        $this->stats  = (array) $this->get('Stats');
+        $this->recent = (array) $this->get('Recent');
+
         if (class_exists(AssetService::class)) {
             (new AssetService())->useComponents($this->getDocument()->getWebAssetManager());
         }
+
+        ToolbarHelper::title(Text::_('COM_XDECARONOTIFICATIONS_DASHBOARD'));
 
         parent::display($tpl);
     }
