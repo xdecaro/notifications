39cfeb7104bfa838b062b168af785718ff289425
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-08 23:54:50 +0200

    Add Notifications administrator view

---
+++ component/admin/src/View/Notifications/HtmlView.php
@@ -0,0 +1,36 @@
+<?php
+namespace Xdecaro\Component\Notifications\Administrator\View\Notifications;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use Joomla\CMS\Toolbar\ToolbarHelper;
+use Xdecaro\Core\Asset\AssetService;
+
+final class HtmlView extends BaseHtmlView
+{
+    /** @var array */
+    public $items = [];
+
+    /** @var mixed */
+    public $pagination;
+
+    /** @var mixed */
+    public $state;
+
+    public function display($tpl = null): void
+    {
+        $this->items      = $this->get('Items');
+        $this->pagination = $this->get('Pagination');
+        $this->state      = $this->get('State');
+
+        if (class_exists(AssetService::class)) {
+            (new AssetService())->useComponents($this->getDocument()->getWebAssetManager());
+        }
+
+        ToolbarHelper::title(Text::_('COM_XDECARONOTIFICATIONS_NOTIFICATIONS'));
+
+        parent::display($tpl);
+    }
+}
