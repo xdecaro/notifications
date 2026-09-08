a88326379ec5b6ff55e0786c16547730224237d9
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-08 23:54:16 +0200

    Register Notifications persistence service

--- component/admin/services/provider.php
+++ component/admin/services/provider.php
@@ -8,6 +8,7 @@ use Joomla\CMS\Extension\ComponentInterface;
 use Joomla\CMS\Extension\Service\Provider\ComponentDispatcherFactory;
 use Joomla\CMS\Extension\Service\Provider\MVCFactory;
 use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
+use Joomla\Database\DatabaseInterface;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
 use Xdecaro\Component\Notifications\Administrator\Extension\NotificationsComponent;
@@ -17,10 +18,26 @@ return new class implements ServiceProviderInterface {
     {
         $container->registerServiceProvider(new MVCFactory('Xdecaro\\Component\\Notifications'));
         $container->registerServiceProvider(new ComponentDispatcherFactory('Xdecaro\\Component\\Notifications'));
+
         $container->share(CoreIntegrationService::class, static fn (): CoreIntegrationService => new CoreIntegrationService());
-        $container->set(ComponentInterface::class, static fn (Container $container): ComponentInterface => new NotificationsComponent(
-            $container->get(ComponentDispatcherFactoryInterface::class),
-            $container->get(MVCFactoryInterface::class)
-        ));
+        $container->share(
+            NotificationService::class,
+            static fn (Container $container): NotificationService => new NotificationService(
+                $container->get(DatabaseInterface::class)
+            )
+        );
+
+        $container->set(
+            ComponentInterface::class,
+            static function (Container $container): ComponentInterface {
+                $component = new NotificationsComponent(
+                    $container->get(ComponentDispatcherFactoryInterface::class),
+                    $container->get(MVCFactoryInterface::class)
+                );
+                $component->setNotificationService($container->get(NotificationService::class));
+
+                return $component;
+            }
+        );
     }
 };
